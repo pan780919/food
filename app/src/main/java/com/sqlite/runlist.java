2a424d66da0e6a7418ebcc92be0a9feae1be56bd
JackPan <jheng.wei0919@gmail.com>
2017-11-30 14:34:31 +0800

    修正計步器 閃退

---
+++ app/src/main/java/com/sqlite/runlist.java
@@ -0,0 +1,9 @@
+package com.sqlite;
+
+public class runlist {
+    public static  final String ID = "";
+    public static  final String TIME = "";
+    public static  final String DATE = "";
+
+
+}
